c31ca1995bbd9e318ca519716da95de12f3b2f38
Tony G <tlguzman94@gmail.com>
2020-06-01 22:26:09 -0400

    refactored App component from class to functionional component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import fetchWeatherData from './services/weather-api';
 import Input from './components/Input';
 import Error from './components/Error';
 import Display from './components/Display';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      city: null,
-      temp: null,
-      feels_like: null,
-      main: null,
-      description: null,
-      error: null,
-    };
-    this.fetchData = this.fetchData.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+const App = () => {
+  const [weatherData, setWeatherData] = useState({});
+  const [error, setError] = useState('');
 
-  async fetchData(name) {
+  const fetchData = async (name) => {
     const data = await fetchWeatherData(name);
     if (data.cod === 200) {
       const {
@@ -30,43 +19,40 @@ class App extends React.Component {
           0: { description, main },
         },
       } = data;
-      this.setState({
+      setWeatherData({
         city: name,
         temp: temp,
         feels_like,
         main,
         description,
-        error: null,
       });
     } else {
       console.log(`Error: ${data.cod} ${data.message}`);
-      this.setState({ error: data.message });
+      setError(data.message);
     }
-  }
+  };
 
-  handleSubmit(event) {
-    const input = event.target.querySelector('#location-input');
-    if (input.value) {
-      this.fetchData(input.value);
+  const handleSubmit = (event) => {
+    const input = event.target.locationInput.value;
+    if (input) {
+      fetchData(input);
       event.target.reset();
     }
     event.preventDefault();
-  }
+  };
 
-  render() {
-    return (
-      <div id="app">
-        <Input onSubmit={this.handleSubmit} />
-        <Error message={this.state.error} />
-        <Display
-          city={this.state.city}
-          temp={this.state.temp}
-          description={this.state.description}
-          main={this.state.main}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div id="app">
+      <Input onSubmit={handleSubmit} />
+      <Error message={error} />
+      <Display
+        city={weatherData.city}
+        temp={weatherData.temp}
+        description={weatherData.description}
+        main={weatherData.main}
+      />
+    </div>
+  );
+};
 
 export default App;
